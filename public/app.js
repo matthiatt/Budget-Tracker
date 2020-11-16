54a1a944705f841e70d9bd24965ae2830970b42e
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-16 17:57:46 -0600

    edit a name, for less confusion. Also added the [first] fetch.

--- public/app.js
+++ public/app.js
@@ -1,5 +1,12 @@
-let transactions = [];
+// Creating an array for later functionality to be implemented by 'transModel'
+let transModel = [];
 
-fetch("/apiroute/transaction").then((res) => {
-  return res.json();
-});
+fetch("/apiroute/transaction")
+  .then((res) => {
+    // Promise to return the the data on response.
+    return res.json();
+  })
+  .then((data) => {
+    // Creating a promise to take the database data on a global variable I called on line 1.
+    transModel = data;
+  });
