cb0f37155b5bb7eebf1afe6e951f5c0723c3cdc1
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-16 02:52:42 -0600

    added post router

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -3,7 +3,20 @@ const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
 router.get("/apiroute/transaction", (req, res) => {
-  Transaction.find({}) //find the object within the model document called.
+  Transaction.find({}) // Find the object within the model document called.
+    .then((TransactionDb) => {
+      res.json(TransactionDb);
+    })
+    .catch((err) => {
+      // Catch error, then return error below.
+      res.status(404).json(err);
+    });
+});
+
+router.post("/apiroute/transaction", ({ body }, res) => {
+  Transaction.create(body) // Calling 'body' from line 16
+    // Creating a body to post data too.
+    // Finding the object within the model document.
     .then((TransactionDb) => {
       res.json(TransactionDb);
     })
