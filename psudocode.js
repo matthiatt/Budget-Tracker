1a4fd03d2856eac25ee3d4dbfb38b8e41b9bf99b
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-15 21:57:55 -0600

    server added

---
+++ psudocode.js
@@ -0,0 +1,6 @@
+// Add in a way to create offline functionality cache to have a way to connect user changes once internet connection is established again.
+// This should work when a user withdrawls funds and/or deposits funds into thier account.
+// The changes should be displayed so the user knows that the task has been completed successfully.
+// The transaction must also show up on the transaction history - once the user has connected to the server again via internet.
+
+// When offline entries are stored, they should be added to a type of tracker.
---
+++ server.js
@@ -0,0 +1,30 @@
+// Required modules
+const express = require("express");
+const mongoose = require("mongoose");
+const compression = require("compression");
+const logger = require("morgan");
+
+const PORT = 3000; // Port number connected
+
+const app = express(); //express being defined
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true })); // So express data can be read
+app.use(express.json());
+
+app.use(express.static("public")); // to the public folder.
+
+mongoose.connect("mongodb://localhost/budgetTracker", {
+  // What I am calling my database.
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+// Routes I am using here
+app.use(require("./routes/apiroute.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
