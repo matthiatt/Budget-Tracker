4e7f54b3fc11ddf316e4ac3606cc4eac4df8cb13
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-16 02:45:34 -0600

    added get router

--- routes/apiroute.js
+++ routes/apiroute.js
@@ -2,4 +2,15 @@
 const router = require("express").Router();
 const Transaction = require("../models/transaction.js");
 
+router.get("/apiroute/transaction", (req, res) => {
+  Transaction.find({}) //find the object within the model document called.
+    .then((TransactionDb) => {
+      res.json(TransactionDb);
+    })
+    .catch((err) => {
+      // Catch error, then return error below.
+      res.status(404).json(err);
+    });
+});
+
 module.exports = router;
