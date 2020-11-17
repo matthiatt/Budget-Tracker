b70a6958d9344582af5af5260406c558ea4d3416
Matthew Hiatt <mhiatt62@gmail.com>
2020-11-17 01:12:35 -0600

    active service-worker complete

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,22 +9,25 @@ const FILES_TO_CACHE = [
   "icons/icon-512x512.png",
 ];
 
-const cacheStatic = "staticCache";
-const cacheDataGiven = "cacheDataName";
+// Changed my last values to a new string.  Got this from class activities example.
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
 
 //Next I want to create/install my service worker here.
 self.addEventListener("install", (e) => {
   // adding an event listener here within the function within the parameters.
   e.waitUntil(
-    caches.open(cacheStatic).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
+    caches
+      .open(STATIC_CACHE)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  ); // Restructured to put the 'skipWaiting()' in a different place.
+  //   self.skipWaiting();
 });
 
 // Next after I installed the service-worker, I now want to activate it so it can function correctly.
 self.addEventListener("active", (e) => {
+  const currentCaches = [STATIC_CACHE, RUNTIME_CACHE];
   e.waitUntil(
     caches.keys().then((cachedItemList) => {
       return cachedItemList.filter(cachedItemList);
